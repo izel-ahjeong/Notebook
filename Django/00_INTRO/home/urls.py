8362c2fb9b9425eba37863a77d94b813b443233c
izel-ahjeong <154478550+izel-ahjeong@users.noreply.github.com>
2024-01-08 14:56:50 +0900

    Add files via upload

---
+++ Django/00_INTRO/home/urls.py
@@ -0,0 +1,15 @@
+
+# home/urls.py (새로만들기)
+
+from django.urls import path
+from . import views
+
+# 반드시 변수명 urlpatterns
+urlpatterns = [
+    # /home/test/
+    path('test/', views.test),
+    # /home/
+    path('', views.index),
+    # /home/about/
+    # /home/contact/
+]
---
+++ Django/00_INTRO/recap/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from datetime import datetime
+import random
+
+def index(request):
+    return render(request, 'recap/index.html')
+
+
+def is_xmas(request):
+    today = datetime.now()
+    if today.month == 12 and today.day == 25:
+        result = 'Yes'
+    else:
+        result = 'No'
+
+    return render(request, 'recap/is_xmas.html', {
+        'result': result,
+    })
+
+
+def lunch(request):
+    menus = ['짜장면', '백반', '샐러드', 
+            '순대국', '샌드위치', '떡볶이',]
+    
+    menu = random.choice(menus)
+
+    return render(request, 'recap/lunch.html', {
+        'menu': menu,
+    })
